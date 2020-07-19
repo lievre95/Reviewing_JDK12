c834a77ce922c2663f4a39a37a2df2ecb7a05cc3
lievre95 <ivandoga95@gmail.com>
2020-07-19 23:51:04 +0300

    modules; requires

---
+++ src/demo/com/dogaion/test/Module.java
@@ -0,0 +1,13 @@
+package com.dogaion.test;
+import javax.swing.JOptionPane;
+public class Module {
+    public static void main(String[] args) {
+JOptionPane.showMessageDialog(null,"Hello boyzz! That's Demo Module");  }
+}
+//in d:\Projects\First\src>...
+//compiling
+//javac demo/module-info.java demo/com/dogaion/test/Module.java
+
+//running
+//java --module-path demo --module demo/com.dogaion.test.Module
+//Hey, boyzz =)
